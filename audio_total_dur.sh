db73f71bfb3b49c2c0ee8493e5af1d0a37ba8a94
vyhuholl <olga-p-98@mail.ru>
2019-10-28 11:49:51 +0300

    Add files via upload

--- audio_total_dur.sh
+++ audio_total_dur.sh
@@ -1,1 +1,2 @@
+chmod 777 "./kz_uz_records/*"
 find "./kz_uz_records/" -name "*.wav" -exec afinfo {} \; | awk '/estimated duration/ { print $3 }' | paste -sd+ - | bc | awk '{printf("%02d:%02d\n",($1/60%60),($1%60))}'
